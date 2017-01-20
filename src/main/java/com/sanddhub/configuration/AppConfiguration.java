25e1e3b1f6d79e982917cf29e9440a63e9aa5943
Jakub Korocinski <jakub.korocinski@gmail.com>
2017-01-20 02:21:55 +0100

    Dodanie nowych plików

---
+++ src/main/java/com/sanddhub/configuration/AppConfiguration.java
@@ -0,0 +1,12 @@
+package com.sanddhub.configuration;
+
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = { "com.*" })
+@ServletComponentScan(basePackages = { "com.*" })
+public class AppConfiguration {
+
+}
